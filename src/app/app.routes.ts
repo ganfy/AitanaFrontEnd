05b9f2b3c171f9ac320ff4372971d724be6350f9
Alexander Gomez Del Carpio <alexander.gomez@ucsp.edu.pe>
2024-09-17 21:25:14 -0500

    First commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent } // Ruta principal
+];
+
+export default routes;
---
+++ src/app/pages/aitana/aitana.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aitana',
+  standalone: true,
+  imports: [],
+  templateUrl: './aitana.component.html',
+  styleUrl: './aitana.component.css'
+})
+export class AitanaComponent {
+
+}
