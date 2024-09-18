b190604311fac22a3edead7def244e913cbab7e7
Alexander Gomez Del Carpio <alexander.gomez@ucsp.edu.pe>
2024-09-18 17:05:47 -0500

    Adding the navbar and Contact-About pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { AitanaComponent } from './pages/aitana/aitana.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent } // Ruta principal
+  { path: '', redirectTo: '/home', pathMatch: 'full' },  //Que tanto debe estar match, en este caso 100%
+  { path: 'home', component: HomeComponent }, // Ruta principal
+  { path: 'Aitana', component: AitanaComponent },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'contact-us', component: ContactUsComponent },
 ];
 
 export default routes;
