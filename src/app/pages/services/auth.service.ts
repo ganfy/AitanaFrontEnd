a46e221c097cd45e7d845a1593e97002a1c29090
Alexander Gomez Del Carpio <alexander.gomez@ucsp.edu.pe>
2024-10-03 22:26:52 -0500

    Login added

--- src/app/pages/services/auth.service.ts
+++ src/app/pages/services/auth.service.ts
@@ -8,10 +8,10 @@ export class AuthService {
   constructor(private http: HttpClient) {}
 
   signUp(username: string, password: string) {
-    return this.http.post('http://localhost:3000/auth/signup', { username, password });
+    return this.http.post('http://localhost:3000/auth/sign-up', { username, password });
   }
 
   login(username: string, password: string) {
-    return this.http.post('http://localhost:3000/auth/login', { username, password });
+    return this.http.post('http://localhost:3000/auth/log-in', { username, password });
   }
 }
