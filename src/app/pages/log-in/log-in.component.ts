14453ed9ce0a4072e5bed050d4f6094dd176e178
Alexander Gomez Del Carpio <alexander.gomez@ucsp.edu.pe>
2024-10-11 09:56:15 -0500

    Mejorar el front

--- src/app/pages/log-in/log-in.component.ts
+++ src/app/pages/log-in/log-in.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../shared/header/header.component';
 
 @Component({
   selector: 'app-log-in',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './log-in.component.html',
   styleUrl: './log-in.component.css'
 })
--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from '../../shared/header/header.component';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, HeaderComponent],
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']  // Enlace del CSS
 })
